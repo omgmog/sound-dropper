de29777eddf4a66fabc34ebd898f046697ef7596
Max Glenister <moglenstar@gmail.com>
2021-05-29 13:17:34 +0100

    Made file writing work, started vue UI
    Co-authored-by: Ben Foxall <benfoxall@gmail.com>

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const slugify = require('slugify');
 const upload = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, 'uploads/')
+            cb(null, 'drops/')
         },
         filename: (req, file, cb) => {
-            cb(null, slugify(file.originalname, {
-                lower: true,
-                remove: /[*+~()'"!:@]/g
-            }));
+            const time = new Date().getTime()
+            const name = slugify(req.body.username || 'anonymous', {remove: /[\\\/*+~.()'"!:@]/g}).slice(0,30);
+
+            const filename = `${time}-${name}.ogg`
+
+            cb(null, filename);
         }
     }),
     fileFilter: (req, file, cb) => {
@@ -26,20 +28,27 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.post('/upload', upload.single('file'), (req, res, next) => {
-    console.log(`Doing something with ${req.file}`);
-    console.log(req);
-});
+app.post('/drop', upload.single('file'), (req, res) => {
+    res.send("thanks")
+})
 
 
-app.get('/upload/:file', (req, res) => {
+app.get('/drops/:file', (req, res) => {
     res.send(`Requested file "${req.params.file}"`);
 })
 
-app.get('/', (req, res) => {
-    res.send('Try POST to /upload, or GET /upload/${filename}')
+app.get('/drops', (req, res) => {
+    res.send([
+        {by: 'max', length: 200},
+        {by: 'ben', length: 120},
+    ])
 })
 
+
+
+// load files in ./static directory
+app.use(express.static('static'))
+
 app.listen(port, () => {
-    console.log(`Listening on ${port}`);
+    console.log(`Listening on http://localhost:${port}`);
 });
