5ffcce02ad0d390282ab5fdcabdcd91a3a1beb5d
Max Glenister <moglenstar@gmail.com>
2021-06-11 20:36:24 +0100

    Set the page title to something more friendly

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,12 @@ module.exports = {
     before: configureAPI,
     port: 3000,
   },
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Sound Dropper';
+        return args;
+      });
+  },
 };
