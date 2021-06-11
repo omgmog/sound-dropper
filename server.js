62d017c8636e44450128a897956d3c976296b1ef
Max Glenister <moglenstar@gmail.com>
2021-06-11 16:22:57 +0100

    Autofix lint errors

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const { resolve } = require('path')
-const history = require('connect-history-api-fallback')
-const express = require('express')
-const configureAPI = require('./server/api.js')
-const app = express()
+const { resolve } = require('path');
+const history = require('connect-history-api-fallback');
+const express = require('express');
+const configureAPI = require('./server/api');
 
-const { PORT = 3000 } = process.env
+const app = express();
+
+const { PORT = 3000 } = process.env;
 
 // API
-configureAPI(app)
+configureAPI(app);
 
 // UI
-const publicPath = resolve(__dirname, 'dist')
-const staticConf = { maxAge: '1y', etag: false }
+const publicPath = resolve(__dirname, 'dist');
+const staticConf = { maxAge: '1y', etag: false };
 
-app.use(express.static(publicPath, staticConf))
-app.use('/', history())
+app.use(express.static(publicPath, staticConf));
+app.use('/', history());
 
 // Go
-app.listen(PORT, () => console.log(`App running on port ${PORT}!`))
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const configureAPI = require('./server/api')
+const configureAPI = require('./server/api');
 
 module.exports = {
   devServer: {
     before: configureAPI,
-    port: 3000
-  }
-}
+    port: 3000,
+  },
+};
